96f3c0f0a2367669a1711c140bc785a4fe59af75
BSODigital <team@bsodigital.com.au>
2022-11-15 14:55:52 +0100

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -133,7 +133,7 @@ export default function Footer() {
             &copy; {new Date().getFullYear()} - Dr Stephen Kleid FRACS. All Rights Reserved.
           </div>
           <div className="right-part">
-            <a className="logo-footer" href="https://cocorubyplasticsurgery.com.au/" target="_blank">
+            <a className="logo-footer" href="https://cocorubyplasticsurgery.com.au/" target="_blank" rel="noopener noreferrer">
               <Image
                 src="/logo-footer.jpg"
                 width={400}
@@ -143,25 +143,25 @@ export default function Footer() {
               />
             </a>
             <div class="social-links"> 
-              <a href="https://www.facebook.com/drstephenkleid" target="_blank">
+              <a href="https://www.facebook.com/drstephenkleid" target="_blank" rel="noopener noreferrer">
                 <FaFacebookF
                   title="Facebook"
                   className={styles['social-icon']}
                 />
               </a>
-              <a href="https://www.instagram.com/drstephenkleid/" target="_blank">
+              <a href="https://www.instagram.com/drstephenkleid/" target="_blank" rel="noopener noreferrer">
                 <FaInstagram
                   title="Instagram"
                   className={styles['social-icon']}
                 />
               </a>
-              <a href="https://www.youtube.com/playlist?list=PL6Jjhwa1iz3XqL6kmz86i4ezFtaHIJTNq" target="_blank">
+              <a href="https://www.youtube.com/playlist?list=PL6Jjhwa1iz3XqL6kmz86i4ezFtaHIJTNq" target="_blank" rel="noopener noreferrer">
                 <FaYoutube
                   title="YouTube"
                   className={styles['social-icon']}
                 />
               </a>
-              <a href="https://www.linkedin.com/in/stephen-kleid-06617038/" target="_blank">
+              <a href="https://www.linkedin.com/in/stephen-kleid-06617038/" target="_blank" rel="noopener noreferrer">
                 <FaLinkedinIn
                   title="LinkedIn"
                   className={styles['social-icon']}
