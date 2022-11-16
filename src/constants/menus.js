8fb7e277bf738411cd39fda805868aaceffa2816
mat <team@bsodigital.com.au>
2022-11-16 14:47:00 +0100

    header menu

--- src/constants/menus.js
+++ src/constants/menus.js
@@ -7,3 +7,5 @@ export const PRIMARY_LOCATION = 'PRIMARY';
  * @type {import('client/schema.generated').MenuLocationEnum}
  */
 export const FOOTER_LOCATION = 'FOOTER';
+
+export const TOP_LOCATION = 'TOP';
