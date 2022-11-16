f4ed06e49e276238aa0201c04177cb7f5795b777
mat <team@bsodigital.com.au>
2022-11-16 12:26:04 +0100

    new update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -21,10 +21,28 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className="container">
-        <NavigationMenu
-          className={styles.nav}
-          menuLocation={MENUS.FOOTER_LOCATION}
-        />
+
+        <div className="footer-columns">
+          <div className="column">
+            <div className="footer-title">Ear, Nose, Throat</div>
+            <div className="footer-content">
+              <p><strong>Masada Medical Centre</strong><br> Level 1, 26 Balaclava Road,<br> East St Kilda, VIC<br> <a href="tel:0390381630">03 9038 1630</a><br> <a href="mailto:skleid@stephenkleid.com">skleid@stephenkleid.com</a></p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="footer-title">RHINOPLASTY</div>
+            <div className="footer-content">
+              <p><strong>Coco Ruby Plastic Surgery</strong><br> 759 Burwood Rd,<br> Hawthorn East, VIC<br> <a href="tel:1300599990">1300 599 990</a><br> <a href="mailto:enquiries@cocoruby.com.au">enquiries@cocoruby.com.au</a></p><p>&nbsp;</p><p><strong>&nbsp;</strong></p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="footer-title">CLINIC HOURS</div>
+            <div className="footer-content">
+              <p style="text-align: center;"><strong>Mon – Fri </strong>9 am – 5 pm</p>
+            </div>
+          </div>
+
+        </div>
 
         <div className={styles.copyright}>
           <div className="left-part">
